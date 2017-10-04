e4352345ab83ba174f73e169a1f9de075f764f92
Ake Tangkananond <iamake@gmail.com>
2017-10-04 13:31:36 +0700

    .

--- src/TSMSApi/Constants.php
+++ src/TSMSApi/Constants.php
@@ -2,8 +2,6 @@
 
 namespace TencentTH\TSMSApi;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\ClientException;
 
 class Constants {
 
@@ -103,8 +101,8 @@ class Constants {
         },
         TRUE
       ),
-      'send-sms-voice' => new Testcase(
-        'Send voice SMS',
+      'send-sms-voice-en' => new Testcase(
+        'Send voice SMS (EN)',
         function () {
           if (!array_key_exists('mobile', $_REQUEST)) {
             return 'Please supply mobile through HTTP params';
@@ -144,6 +142,111 @@ class Constants {
         '1) Parameters are not consistent. Missing type, senderid, and extend
 2) No pause between repeating the numbers
 3) Occasionally fail to work'
+      ),
+      'send-sms-voice-th' => new Testcase(
+        'Send voice SMS (TH)',
+        function () {
+          if (!array_key_exists('mobile', $_REQUEST)) {
+            return 'Please supply mobile through HTTP params';
+          }
+          $now = new \DateTime();
+          $random = rand();
+          $nonce = 'nonce:' . rand();
+          $nationcode = "66";
+          $mobile = $_REQUEST['mobile'];
+          $message = $now->format('his');
+          $sig = hash('sha256', sprintf(
+            'appkey=%s&random=%d&time=%d&mobile=%s',
+            self::$APP_KEY,
+            $random,
+            $now->getTimestamp(),
+            $mobile
+          ));
+          $url = 'http://tsms.qq.com/Qsms/BackendSendSmsIVR?' . http_build_query([
+              'sdkappid' => self::$APP_ID,
+              'random' => $random
+            ]);
+
+          return APIRequest::create($url, [
+            'tel' => [
+              'nationcode' => $nationcode,
+              'mobile' => $mobile,
+            ],
+            'msg' => $message,
+            'playtimes' => 2,
+            'lang' => 'th',
+            'sig' => $sig,
+            'time' => $now->getTimestamp(),
+            'ext' => $nonce
+          ])->execute();
+        },
+        FALSE,
+        '1) Parameters are not consistent. Missing type, senderid, and extend
+2) The repeating numbers has no effect
+3) Occasionally fail to work'
+      ),
+      'status-mt' => new Testcase(
+        'Status MT',
+        function () {
+          if (!array_key_exists('messageid', $_REQUEST)) {
+            return 'Please supply messageid through HTTP params';
+          }
+          $now = new \DateTime();
+          $random = rand();
+          $messageid = $_REQUEST['messageid'];
+          $sig = hash('sha256', sprintf(
+            'appkey=%s&random=%d&time=%d&messageid=%s',
+            self::$APP_KEY,
+            $random,
+            $now->getTimestamp(),
+            $messageid
+          ));
+          $url = 'http://tsms.qq.com/Qsms/BackendSmsReportQuery?' . http_build_query([
+              'sdkappid' => self::$APP_ID,
+              'random' => $random
+            ]);
+
+          return APIRequest::create($url, [
+            'messageid' => $messageid,
+            'sig' => $sig,
+            'time' => $now->getTimestamp(),
+          ])->execute();
+        },
+        FALSE,
+        '1) Parameters are not consistent. Missing ext
+2) Time is not in standard format e.g. ISO-8601
+3) Status is invalid. I have not got a call IVR but the status is DELIVRD'
+      ),
+      'status-number' => new Testcase(
+        'Status Number',
+        function () {
+          if (!array_key_exists('mobile', $_REQUEST)) {
+            return 'Please supply mobile through HTTP params';
+          }
+          $now = new \DateTime();
+          $random = rand();
+          $mobile = $_REQUEST['mobile'];
+          $sig = hash('sha256', sprintf(
+            'appkey=%s&random=%d&time=%d&mobile=%s',
+            self::$APP_KEY,
+            $random,
+            $now->getTimestamp(),
+            $mobile
+          ));
+          $url = 'http://tsms.qq.com/Qsms/BackendQueryNisInfo?' . http_build_query([
+              'sdkappid' => self::$APP_ID,
+              'random' => $random
+            ]);
+
+          return APIRequest::create($url, [
+            'mobile' => $mobile,
+            'sig' => $sig,
+            'time' => $now->getTimestamp(),
+          ])->execute();
+        },
+        FALSE,
+        '1) Validity is not correct, unregistered mobile phone number returned as valid e.g. 66867605527
+2) Carrier name is not valid'
       ),
     ];
   }
